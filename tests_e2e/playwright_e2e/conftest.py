7d868ab42b03fc6bf02f69098d0864a493f7c005
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-08-23 11:02:19 +0100

    feat(e2e): check webapp is available before attempting e2e run

--- tests_e2e/playwright_e2e/conftest.py
+++ tests_e2e/playwright_e2e/conftest.py
@@ -1,7 +1,7 @@
 # >>> pytest tests_e2e/playwright_e2e/
 import pytest
 from environs import Env
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Browser, Playwright, sync_playwright
 
 env = Env()
 env.read_env()
@@ -9,14 +9,35 @@ env.read_env()
 BASE_URL = "http://127.0.0.1:8000"
 
 
+def _launch_browser(headless: bool = True) -> tuple[Playwright, Browser]:
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=headless)
+    return playwright, browser
+
+
+def pytest_sessionstart(session):
+    """"""
+    try:
+        playwright, browser = _launch_browser(True)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto(BASE_URL, timeout=3000)
+    except Exception as e:
+        assert 0
+        pytest.exit(f"Startup check failed: {e}", returncode=1)
+    finally:
+        context.close()
+        browser.close()
+        playwright.stop()
+
+
 @pytest.fixture(scope="session")
 def browser():
     """Start Playwright Chromium once per test session."""
-    playwright = sync_playwright().start()
     is_ci = env.bool("CI", default=False)
     is_gha = env.bool("GITHUB_ACTIONS", default=False)
     headless = is_ci or is_gha
-    browser = playwright.chromium.launch(headless=headless)
+    playwright, browser = _launch_browser(headless)
     yield browser
     browser.close()
     playwright.stop()
